6158745a55897b3acf0beeb5d004ca9a83743eac
Botmasher <languagejosh@hotmail.com>
2017-05-06 10:49:36 -0400

    Build towards name generator class

--- Main.cs
+++ Main.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 
 class Main {
 	
+	// model sound structure
+	public class Inventory {
+		string nasals = "";
+		string plosivesVoiced = "";
+		string plosivesVoiceless = "";
+		string fricativesVoiced = "";
+		string fricativesVoiceless = "";
+		string approximants = "";
+		string vowels = "";
+
+		public Inventory () {
+		}
+		public void AddNasals (string nasals) {
+			this.nasals = String.Format("{0}{1}", this.nasals, nasals);
+		}
+		public void AddPlosives (string plosives) {
+			this.plosivesVoiced = String.Format("{0}{1}", this.plosivesVoiced, plosives);
+		}
+		public void AddPlosivesVoiceless (string plosives) {
+			this.plosivesVoiceless = String.Format("{0}{1}", this.plosivesVoiceless, plosives);
+		}
+		public void AddFricativesVoiced (string fricatives) {
+			this.fricativesVoiced = String.Format("{0}{1}", this.fricativesVoiced, fricatives);
+		}
+		public void AddFricativesVoiceless (string fricatives) {
+			this.fricativesVoiceless = String.Format("{0}{1}", this.fricativesVoiceless, fricatives);
+		}
+		public void AddApproximants (string approximants) {
+			this.approximants = String.Format("{0}{1}", this.approximants, approximants);
+		}
+		public void AddVowels (string vowels) {
+			this.vowels = String.Format("{0}{1}", this.vowels, vowels);
+		}
+	}
+
+
 	// model syllable structure as onset, nucleus, coda
 	public class Syllable {
 		List<string> nuclei = new List<string>();
 		List<string> onsets = new List<string>();
 		List<string> codas = new List<string>();
-		public Syllable (List<string> onsets, List<string> nuclei, List<string> codas) {
-			this.onsets = onsets;
-			this.nuclei = nuclei;
-			this.codas = codas;
+
+		public Syllable () {
 		}
 		public void AddOnset (string onsets) {
 			this.onsets.Add(onset);
@@ -24,24 +58,37 @@ class Main {
 		}
 	}
 
-	// build and store a syllable
-	public class SyllableGenerator {
-		Syllable syllParts;
-		public SyllableGenerator (syllable) {
-			syllParts = syllable;
+
+	// build and store a syllable (modeled as process -ation rather than entity -ator)
+	public class NameGeneration {
+		Syllable syllable;
+		Inventory inventory;
+		Dictionary<string,string> affixes = new Dictionary<string,string>();
+		// sound change kvs of structure 'feature, feature' -> 'feature, feature'
+		Dictionary<string,string> soundChanges = new Dictionary<string,string>();
+
+		public NameGeneration () {
+			this.syllable = new Syllable();
+			this.inventory = new Inventory();
 		}
-		void BuildSyllable () {
+		public void BuildSyllable () {
 			// recipe
-			// - rules
-			// - features
+			// ?- internal rules
+			// - build by features
 			// - roll for each part
 			// - choose parts
-			// - build syllable
-			// - clean syllable (assim., etc.)
+			//
+			// - attach syllables->word
+			//
+			// - word affixes
+			// - word-internal changes
+			// - word-edge changes
 		}
 	}
 
 	public static void Main(string[] args) {
+
 		Console.WriteLine ("test print");
+
 	}
 }
