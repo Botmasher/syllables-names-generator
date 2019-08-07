043710f30cea21999785e7b403b74aedbf5e2eae
Botmasher <languagejosh@hotmail.com>
2019-08-07 00:16:42 -1000

    Add Morae check, fetch, reduce methods and flip beat-morae mapping

--- languagebuilder/phonology/morae.py
+++ languagebuilder/phonology/morae.py
@@ -1,18 +1,39 @@
 class Morae:
     def __init__(self, phonology):
         self.phonology = phonology
-        self.morae = {}     # map of morae sets per beats count
-        
-    def set_mora(self, sounds_or_features, beats=1, overwrite=False):
+        self.morae = {}     # map of beat counts per mora
+    
+    def set_beats(self, sounds_or_features, beats=1, overwrite=False):
         """Add a moraic structure and its associated beat count to the stored morae"""
         if not isinstance(beats, (int, float)):
             raise TypeError(f"Morae expected beat count to be a number not {beats}")
 
         vetted_mora = self.vet_mora(sounds_or_features)
         
-        # add mora to single-count set
-        self.morae.setdefault(beats, set()).add(vetted_mora)
+        # map mora and associated beats
+        if overwrite or vetted_mora not in self.morae:
+            self.morae[vetted_mora] = beats
         return vetted_mora
+
+    def count_beats(self, mora):
+        return self.morae.get(mora)
+
+    def is_mora(self, mora):
+        return mora in self.morae
+
+    def reduce(self, iterable, expression, starting_value):
+        accumulator = starting_value
+        for current_value in iterable:
+            accumulator = expression(current_value, accumulator)
+        return accumulator
+
+    def reduce_beats(self):
+        """Remap morae data from beats per moraic entry into sets of morae per beat"""
+        return self.reduce(
+            self.morae.keys(),
+            lambda mora, morae_per_beat: morae_per_beat.setdefault(self.morae[mora], set()).add(mora),
+            {}
+        )
 
     def vet_mora(self, sounds_or_features):
         """Turn a list of sounds or features lists or strings into a list of lists of
@@ -45,12 +66,13 @@ class Morae:
         ]
         current_mora = []
         count = 0
+        morae_per_beat = self.reduce_beats()
         for features in morae_features:
             current_mora.append(features)
-            # TODO: check for existence of any subset of features in dict lists
-            for mora_count in self.morae:
-                if current_mora in self.morae[mora_count]:
-                    count += mora_count
+            for beats in morae_per_beat:
+                # TODO: check for existence of any subset of features
+                if current_mora in morae_per_beat[beats]:
+                    count += beats
                     current_mora = []
         if current_mora:
             return
